484e5649a762393be736b1f372c1c64c3c2728d3
Kiwamu Okabe <kiwamu@debian.or.jp>
2012-10-13 14:46:47 +0900

    Use template file on cgi always

--- src/Web/All.hs
+++ src/Web/All.hs
@@ -2,11 +2,17 @@
 module Web.All(action) where
 
 import CmdLine.All
+import General.Base
 import General.Web
 import Web.Server
 import Web.Response
+import Web.Page
+import Paths_hoogle
 
 
 action :: CmdLine -> IO ()
 action q@Server{} = server q
-action q = cgiResponse =<< response responseArgs q
+action q = do
+  f <- readFile' =<< getDataFileName ("resources" </> "template" <.> "html")
+  let t = loadTemplates f
+  cgiResponse =<< response responseArgs{templates=t} q
